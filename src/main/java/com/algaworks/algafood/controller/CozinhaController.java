564313b8bc9566771e94c3e15134548a6a51a594
Fernandeeess <contatomatheusfernandeeess@gmail.com>
2024-11-20 08:16:12 -0300

    uso do BeanUtils.copyProperties

--- src/main/java/com/algaworks/algafood/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/controller/CozinhaController.java
@@ -3,6 +3,7 @@ package com.algaworks.algafood.controller;
  
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.annotation.AccessType;
 import org.springframework.http.HttpHeaders;
@@ -19,6 +20,11 @@ import org.springframework.web.bind.annotation.RestController;
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.model.CozinhaXmlWrapper;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
 
 @RestController
 @RequestMapping("/cozinhas")
@@ -38,14 +44,29 @@ public class CozinhaController {
 	@GetMapping("/{cozinhaId}")
 	public ResponseEntity<Cozinha> buscar(@PathVariable("cozinhaId") Long id) {
 		Cozinha cozinha = cozinhaRepository.buscar((Long)id);
-		//return ResponseEntity.ok(cozinha);
+		return ResponseEntity.ok(cozinha);
 
-		HttpHeaders headers = new HttpHeaders();
-		headers.add(HttpHeaders.LOCATION, "http://api.algafood.local:8080/cozinhas"); //Redirecionamento
+		// HttpHeaders headers = new HttpHeaders();
+		// headers.add(HttpHeaders.LOCATION, "http://api.algafood.local:8080/cozinhas"); //Redirecionamento
 
-		return ResponseEntity.status(HttpStatus.FOUND).headers(headers).build();
+		// return ResponseEntity.status(HttpStatus.FOUND).headers(headers).build();
 		 
 	}
+	@PostMapping
+	public ResponseEntity<Cozinha> Adicionar(@RequestBody Cozinha cozinhaBody) {
+		Cozinha cozinha = cozinhaRepository.salvar(cozinhaBody);
+		return ResponseEntity.status(HttpStatus.CREATED).body(cozinha);
+	}
+	@PutMapping("/{id}")
+	public ResponseEntity<Cozinha> atualizar(@PathVariable Long id, @RequestBody Cozinha cozinhaBody) {
+		Cozinha cozinhaEntity = cozinhaRepository.buscar(id);
+		BeanUtils.copyProperties(cozinhaBody, cozinhaEntity,"id"); //Copiar propriedades de umas instancia para a  outra, terceiro parametro serve para definir o valor que sera ignorado
+
+		cozinhaEntity = cozinhaRepository.salvar(cozinhaEntity);
+
+		return ResponseEntity.status(HttpStatus.OK).body(cozinhaEntity);
+	}
+	
 	
 	
 }

8717f587c5d06eb4a5b3fc063ec2529302e5d94c
Fernandeeess <contatomatheusfernandeeess@gmail.com>
2024-11-20 07:57:30 -0300

    Uso de Redirecionamento 302

--- src/main/java/com/algaworks/algafood/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/controller/CozinhaController.java
@@ -1,8 +1,14 @@
 package com.algaworks.algafood.controller;
 
+ 
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.annotation.AccessType;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -11,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.model.CozinhaXmlWrapper;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
 
 @RestController
@@ -24,9 +31,20 @@ public class CozinhaController {
 	public List<Cozinha> listar() {
 		return cozinhaRepository.listar();
 	}
+	@GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
+	public CozinhaXmlWrapper listarXML() {
+		return new CozinhaXmlWrapper(cozinhaRepository.listar());
+	}
 	@GetMapping("/{cozinhaId}")
-	public Cozinha buscar(@PathVariable("cozinhaId") Long id) {
-		return cozinhaRepository.buscar((Long)id);
+	public ResponseEntity<Cozinha> buscar(@PathVariable("cozinhaId") Long id) {
+		Cozinha cozinha = cozinhaRepository.buscar((Long)id);
+		//return ResponseEntity.ok(cozinha);
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.add(HttpHeaders.LOCATION, "http://api.algafood.local:8080/cozinhas"); //Redirecionamento
+
+		return ResponseEntity.status(HttpStatus.FOUND).headers(headers).build();
+		 
 	}
 	
 	
